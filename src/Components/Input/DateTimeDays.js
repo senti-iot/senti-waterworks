d5342ac132a212f917d351c8a521c74dcfca70d5
Andrei Tudor <andrei.tudor27@gmail.com>
2019-11-08 16:00:25 +0100

    Day Buttons

--- src/Components/Input/DateTimeDays.js
+++ src/Components/Input/DateTimeDays.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { ItemG } from 'Components'
-import { useLocalization } from 'Hooks'
+import { useLocalization, useDispatch } from 'Hooks'
 import styled from 'styled-components';
 import { Button } from '@material-ui/core';
+import { changeDate } from 'Redux/dateTime';
+import moment from 'moment';
 
 const DayOption = styled(Button)`
 	text-transform: none;
@@ -10,11 +12,16 @@ const DayOption = styled(Button)`
 
 const DateTimeDays = () => {
 	const t = useLocalization()
+	const dispatch = useDispatch()
+	const setDateTime = (menuId, to, from, timeType) => () => {
+		// console.log(menuId, to, from, timeType)
+		dispatch(changeDate(menuId, to, from, timeType))
+	}
 	return (
 		<ItemG container style={{ width: 'auto' }} alignItems={'center'}>
-			<DayOption>{t('filters.dateOptions.7days')}</DayOption>
-			<DayOption>{t('filters.dateOptions.month')}</DayOption>
-			<DayOption>{t('filters.dateOptions.year')}</DayOption>
+			<DayOption onClick={setDateTime(2, moment(), moment().subtract(7, 'day'), 2)}>{t('filters.dateOptions.7days')}</DayOption>
+			<DayOption onClick={setDateTime(3, moment(), moment().startOf('month'), 3)}>{t('filters.dateOptions.month')}</DayOption>
+			<DayOption onClick={setDateTime(4, moment(), moment().startOf('year'), 4)}>{t('filters.dateOptions.year')}</DayOption>
 		</ItemG>
 	)
 }
