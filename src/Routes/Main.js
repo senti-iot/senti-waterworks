4bd47e377d5001a197b76755e9e5ed9905d608f5
Andrei Tudor <andrei.tudor27@gmail.com>
2020-01-27 11:22:43 +0100

    Check Media Queries

--- src/Routes/Main.js
+++ src/Routes/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 // import { Link, /* Router */ } from '@reach/router';
 // import Header from 'Components/Header';
 // import { LocalizationProvider } from 'App';
@@ -14,9 +14,24 @@ import Login from './Login';
 import * as themes from 'Styles/themes'
 // import { darkTheme } from 'variables/themes';
 import NewContent from 'Components/Loaders/NewContent';
+import size from 'Styles/themes/mediaQueries';
 
 
 function Main() {
+	useEffect(() => {
+		//DEV Match Media
+		let mqlsD = Object.keys(size.down).map(key => window.matchMedia(size.down[key]))
+		let mqlsU = Object.keys(size.down).map(key => window.matchMedia(size.up[key]))
+		mqlsD = mqlsD.filter(f => f.matches === true)
+		mqlsU = mqlsU.filter(f => f.matches === true)
+		console.log("Down", mqlsD.map(m => m.media))
+		console.log("Up", mqlsU.map(m => m.media))
+		// for (var i = 0; i < mqls.length; i++) { // loop through queries
+		// 	mediaqueryresponse(mqls[i]) // call handler function explicitly at run time
+		// 	mqls[i].addListener(mediaqueryresponse) // call handler function whenever the media query is triggered
+		// }
+		//eslint-disable-next-line
+	}, [])
 	const colorTheme = useSelector(s => s.settings.colorTheme)
 	return (
 
