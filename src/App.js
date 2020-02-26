2a70cafa2852c6d70b02228f9780e1f1fadc2ed7
Andrei Tudor <andrei.tudor27@gmail.com>
2020-02-26 15:42:43 +0100

    Hot Reloading

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
-import React, { useState } from 'react';
-import Main from 'Routes/Main';
+import React, { useState } from 'react'
+import Main from 'Routes/Main'
 import './App.css'
-import LocalizationProvider from 'Components/Providers/LocalizationProvider';
-import store from 'Redux/store';
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom';
-import { StylesProvider } from "@material-ui/styles";
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import MomentUtils from '@date-io/moment';
 
 export const HTitle = React.createContext(null)
 
 function App() {
 	const [title, setTitle] = useState('')
 	return (
-		<Router>
-			<StylesProvider injectFirst>
-				<Provider store={store}>
-					<LocalizationProvider>
-						<MuiPickersUtilsProvider utils={MomentUtils}>
-							<HTitle.Provider value={setTitle}>
 
-								<Main title={title} />
-							</HTitle.Provider>
-						</MuiPickersUtilsProvider>
-					</LocalizationProvider>
-				</Provider>
-			</StylesProvider>
-		</Router>
-	);
+		<HTitle.Provider value={setTitle}>
+			<Main title={title} />
+		</HTitle.Provider>
+
+	)
 }
+/* test */
 App.whyDidYouRender = true
 
-export default App;
+export default App
---
+++ src/Providers.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './App.css'
+import LocalizationProvider from 'Components/Providers/LocalizationProvider'
+import configureStore from 'Redux/store'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { StylesProvider } from "@material-ui/styles"
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import MomentUtils from '@date-io/moment'
+import { hot } from 'react-hot-loader/root'
+
+export const HTitle = React.createContext(null)
+export const store = configureStore()
+
+const Providers = props => {
+	return (
+		<Router>
+			<StylesProvider injectFirst>
+				<Provider store={store}>
+					<LocalizationProvider>
+						<MuiPickersUtilsProvider utils={MomentUtils}>
+							{props.children}
+						</MuiPickersUtilsProvider>
+					</LocalizationProvider>
+				</Provider>
+			</StylesProvider>
+		</Router>
+	)
+}
+const HotProviders = hot(Providers)
+
+export default HotProviders
