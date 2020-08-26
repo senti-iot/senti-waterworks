8fb17e1cf570ca357dce579a1b64529bb30dd408
Andrei <at@senti.dk>
2020-08-27 01:03:10 +0200

    Fix the year

--- src/Components/Input/DateTimeDays.js
+++ src/Components/Input/DateTimeDays.js
@@ -31,7 +31,7 @@ const DateTimeDays = () => {
 			{uInternal?.sentiWaterworks?.timeType === 1 ? <DayOption isSelected={period.menuId === 0} onClick={setDateTime(0, moment().endOf('day'), moment().startOf('day'), 1)}>{t('filters.dateOptions.today')}</DayOption> : null}
 			<DayOption isSelected={period.menuId === 2} onClick={setDateTime(2, moment().startOf('day'), moment().subtract(7, 'day').startOf('day'), 2)}>{t('filters.dateOptions.7days')}</DayOption>
 			<DayOption isSelected={period.menuId === 3} onClick={setDateTime(3, moment().startOf('day'), moment().startOf('month').startOf('day'), 2)}>{t('filters.dateOptions.month')}</DayOption>
-			<DayOption isSelected={period.menuId === 4} onClick={setDateTime(4, moment().startOf('day'), moment().startOf('year').startOf('day'), 2)}>{t('filters.dateOptions.year')}</DayOption>
+			<DayOption isSelected={period.menuId === 4} onClick={setDateTime(4, moment().startOf('day'), moment().startOf('year').startOf('day'), 4)}>{t('filters.dateOptions.year')}</DayOption>
 		</ItemG>
 	)
 }
