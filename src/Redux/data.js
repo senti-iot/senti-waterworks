dc3f0c98a666a5b67d284ca2a63f322d791db89c
Andrei Tudor <andrei.tudor27@gmail.com>
2021-06-17 04:52:47 +0200

    Legacy Fix

--- src/Redux/data.js
+++ src/Redux/data.js
@@ -358,7 +358,7 @@ export const userData = () =>
 		// 	average: []
 		// }
 
-		let waterUsageData = timeType > 1 ? await getWaterUsageByDay(from, to, [deviceUUID]) : await getWaterUsageByHour(from, to, [deviceUUID])
+		let waterUsageData = timeType > 1 ? await getWaterUsageByDay(from, to, deviceUUID ? [deviceUUID] : null) : await getWaterUsageByHour(from, to, deviceUUID ? [deviceUUID] : null)
 		let waterUsagePrevData = timeType > 1 ? await getWaterUsageByDay(prevFrom, prevTo) : await getWaterUsageByHour(prevFrom, prevTo)
 		let readingsData = await getReadingUsage(from/* .clone().add(1, 'day') */, to)
 		let benchmarkData = timeType > 1 ? await getBenchmarkUsageByDay(orgId, from, to) : await getBenchmarkUsageByHour(orgId, from, to)
