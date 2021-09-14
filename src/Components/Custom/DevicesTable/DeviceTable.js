084ce308c52fc5096cac35e52269248aa1f3e96b
Andrei Tudor <andrei.tudor27@gmail.com>
2021-09-14 21:08:01 +0200

    Export

--- src/Components/Custom/DevicesTable/DeviceTable.js
+++ src/Components/Custom/DevicesTable/DeviceTable.js
@@ -73,7 +73,6 @@ const DeviceTable = (props) => {
 		else {
 			newSDevices.push(device)
 		}
-		console.log('newSDevices', newSDevices)
 		setSelDev(newSDevices)
 	}
 	const selectAllDevices = (s) => {
